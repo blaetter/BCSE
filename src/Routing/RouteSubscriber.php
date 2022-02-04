75d221dc20db6f4ebd8ee2a5f87b3e28fb42c54f
ambo <andreas.maengel@gmail.com>
2022-02-04 17:11:19 +0100

    CS fixes

--- src/Routing/RouteSubscriber.php
+++ src/Routing/RouteSubscriber.php
@@ -9,24 +9,28 @@ use Drupal\blaetter_bcse\Plugin\Search\Search as BcseSearch;
 /**
  * Listens to the dynamic route events.
  */
-class RouteSubscriber extends RouteSubscriberBase {
+class RouteSubscriber extends RouteSubscriberBase
+{
 
-  /**
-   * {@inheritdoc}
-   */
-  public function alterRoutes(RouteCollection $collection) {
+    /**
+     * {@inheritdoc}
+     */
+    public function alterRoutes(RouteCollection $collection)
+    {
 
-    // Alter search page controller for this module's plugin.
-    /** @var \Drupal\search\SearchPageRepositoryInterface $searchPageRepository */
-    $searchPageRepository = \Drupal::service('search.search_page_repository');
-    foreach ($searchPageRepository->getActiveSearchPages() as $entity_id => $entity) {
-      if ($entity->getPlugin() instanceof BcseSearch &&
-        $route = $collection->get("search.view_$entity_id")
-      ) {
-        $route->setDefault('_controller',
-          'Drupal\blaetter_bcse\Controller\BcseSearchController::view');
-      }
+        // Alter search page controller for this module's plugin.
+        /** @var \Drupal\search\SearchPageRepositoryInterface $searchPageRepository */
+        $searchPageRepository = \Drupal::service('search.search_page_repository');
+        foreach ($searchPageRepository->getActiveSearchPages() as $entity_id => $entity) {
+            if (
+                $entity->getPlugin() instanceof BcseSearch &&
+                $route = $collection->get("search.view_$entity_id")
+            ) {
+                $route->setDefault(
+                    '_controller',
+                    'Drupal\blaetter_bcse\Controller\BcseSearchController::view'
+                );
+            }
+        }
     }
-  }
-
 }
